6262578d3ae1244c8a9e418263728addea38d727
tanavburs <84454528+tanavburs@users.noreply.github.com>
2023-08-11 19:01:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+  }
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+  }
